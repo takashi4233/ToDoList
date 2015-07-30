4e12c77a2b57d4d2a786af7a7811d6e70f8da0bb
Yoshimura Takashi <takashi4233@gmail.com>
2015-07-30 22:17:15 +0900

    ToDoリスト完成

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+require('crash-reporter').start();
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+		if (process.platform != 'darwin')
+			app.quit();
+	});
+
+app.on('ready', function() {
+		mainWindow = new BrowserWindow({width: 320, height: 550});
+		mainWindow.loadUrl('file://' + __dirname + '/index.html');
+		mainWindow.on('closed', function() {
+				mainWindow = null;
+			});
+	});
